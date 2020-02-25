8b2d5c29228b9d9e79e85f1d5581e5f48ff26b8b
Victor Hugo Olvera Cruz <volvera@digitalairstrike.com>
2020-02-25 16:24:06 -0600

    Added PropTypes

--- weather-app/src/components/WeatherTemperature.js
+++ weather-app/src/components/WeatherTemperature.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import WeatherIcons from 'react-weathericons';
+import PropTypes from 'prop-types';
 import {
     CLOUD, 
     CLOUDY,
@@ -37,5 +38,11 @@ const WeatherTemperature = ({ temperature, weatherState }) => (
         <span>{`${temperature} C°`}</span>
     </div>
 );
+
+//Definition of PropTypes, parameters and types
+WeatherTemperature.propTypes = {
+    temperature: PropTypes.number,
+    weatherState: PropTypes.string.isRequired
+};
 
 export default WeatherTemperature;
