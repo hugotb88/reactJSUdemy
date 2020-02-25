08770d343f622d499dde772cc22373675a063372
Victor Hugo Olvera Cruz <volvera@digitalairstrike.com>
2020-02-25 15:39:08 -0600

    PAss parameter to weatherState, that determines the icon

--- weather-app/src/components/WeatherTemperature.js
+++ weather-app/src/components/WeatherTemperature.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import WeatherIcons from 'react-weathericons';
 
+
+//Dictionary for icons names
+const icons = {
+    sunny: "day-sunny",
+    fog: "day-fog"
+};
+
+//Function to get icon according the weather state received
+const getWeatherIcon = weatherState => {
+    const icon = icons[weatherState];
+
+    if(icon)
+        return <WeatherIcons name={icon} size="2x" />;
+    else
+        return <WeatherIcons name={"sleet"} size="2x"/>
+};
+
 const WeatherTemperature = ({ temperature, weatherState }) => (
     <div>
-        <WeatherIcons name="day-sunny" size="2x" />
+        {
+            getWeatherIcon(weatherState)
+        }
         <span>{`${temperature} C°`}</span>
     </div>
 );
