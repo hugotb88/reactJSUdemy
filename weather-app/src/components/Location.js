24d0afab320e87530f8b8ff0f8caec5882e58ba5
Victor Hugo Olvera Cruz <volvera@digitalairstrike.com>
2020-02-25 12:18:54 -0600

    Destructuring used, removed curly braces because we have only one line in Location

--- weather-app/src/components/Location.js
+++ weather-app/src/components/Location.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
-const Location = (props) => {
-    const city = props.city;
-
-    return (
-        <div>
-            <h1>{city}</h1>
-        </div>
-    );
-};
+const Location = ({city}) => (
+    <div>
+        <h1>{city}</h1>
+    </div>
+);
 
 export default Location;
