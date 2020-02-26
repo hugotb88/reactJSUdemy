0400398d382d0b83e79ea1748a2a451187fde719
Victor Hugo Olvera Cruz <volvera@digitalairstrike.com>
2020-02-26 17:01:42 -0600

    Use React methods, Constructor, this.state, super, render(), return and destructuring to get properties from the state

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import {
-    SUN
+    SUN, WINDY
   }  from './../../constants/weathers';
 import './styles.css';
 
@@ -13,12 +13,47 @@ const data = {
     wind: '10 m/s'
 }
 
+const data2 = {
+    temperature: 40,
+    weatherState: WINDY,
+    humidity: 14,
+    wind: '19 m/s'
+}
+
+//Class Component
 class WeatherLocation extends Component {
+    //When the component is created
+    constructor(){
+        //always invoked
+        super();
+
+        //Partial state of our component
+        //this.state only in constructor
+        this.state = {
+            city: "Buenos Aires",
+            data: data,
+        };
+    }
+
+    handleUpdateClick = () => {
+        console.log("Updated");
+
+        //For other functions, we should use this.setState instead of this.state
+        this.setState({
+            city: "Mexico City",
+            data: data2,
+        });
+    }
+
     render() {
+        //Using destructuring
+        const { city, data } = this.state;
+
         return (
             <div className= "weatherLocationCont">
-                <Location city={"Barcelona"}></Location>
+                <Location city={city}></Location>
                 <WeatherData data={data}></WeatherData>
+                <button onClick={this.handleUpdateClick}>Actualizar</button>
             </div>
         );
     }
