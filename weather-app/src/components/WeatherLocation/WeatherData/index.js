3402c012480545144ecb093e06232e165adfcef4
Victor Hugo Olvera Cruz <volvera@digitalairstrike.com>
2020-02-26 16:17:08 -0600

    Double destructuring

--- weather-app/src/components/WeatherLocation/WeatherData/index.js
+++ weather-app/src/components/WeatherLocation/WeatherData/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
-import {
-    SUN
-  }  from './../../../constants/weathers';
 import './styles.css';
 
-const WeatherData = () => (
-    <div className="weatherDataCont">
-        <WeatherTemperature 
-        temperature = {20} 
-        weatherState = {SUN}/>
-        
-        <WeatherExtraInfo 
-        humidity={80} 
-        wind={"10 m/s"}/>
+const WeatherData = ({ data: { temperature, weatherState, humidity, wind } }) => (   
+        <div className="weatherDataCont">
+            <WeatherTemperature 
+            temperature = {temperature} 
+            weatherState = {weatherState}/>
+            
+            <WeatherExtraInfo 
+            humidity={humidity} 
+            wind={wind}/>
 
-    </div> 
+        </div> 
 );
 
 
